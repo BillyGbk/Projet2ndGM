a49bbcab39e882ed17f458b7dc0c30759f8e47e8
BillyGbk <52207142+BillyGbk@users.noreply.github.com>
2019-07-02 08:38:07 +0200

    Delete index.php

--- Projet-Billel 2ndGuerre/Projet-Billel 2ndGuerre/code/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "PHP/Controller/Settings.class.php";
-//on récupere les paramètres de l'application
-
-Settings::init();
-// Route va gérer tous les affichages de page en fonction de la provenance
-include ($_SERVER["DOCUMENT_ROOT"] . Settings::getServerRoot());
-?>
